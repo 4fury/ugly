6b59e9113d58531cb7369c3a68aedc2878907daa
4fury <vanuatooshato@gmail.com>
2020-09-26 22:06:29 +0300

    cponysay.

---
+++ ponysay/cponysay/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+
+def build():
+	cmaketools.make()
+
+def install():
+	pisitools.dobin("build/cponysay")
+
