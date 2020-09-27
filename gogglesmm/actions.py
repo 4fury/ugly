8d1792b6a37fb29e99155aa8111d7f21f94159f7
4fury <vanuatooshato@gmail.com>
2020-09-27 08:03:21 +0300

    goggles music manager.

---
+++ gogglesmm/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+j = "-DWITH_ALSA=ON"
+
+t = "_build"
+
+def setup():
+	shelltools.makedirs(t)
+	shelltools.cd(t)
+	cmaketools.configure(j, sourceDir = '..')
+
+def build():
+	shelltools.cd(t)
+	cmaketools.make()
+
+def install():
+	shelltools.cd(t)
+	cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+	pisitools.dodoc("../AUTHORS", "../ChangeLog", "../README")
+
